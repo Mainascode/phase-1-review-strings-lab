4efabc65093ca83d2353d63be27141dc9d32f38b
Maina <smurkcarter855@gmail.com>
2024-09-23 12:28:44 +0300

    origin

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = 'Emmanuel';
+console.log(currentUser)
+const welcomeMessage = "Welcome to Flatbook, Emmanuel !"
+console.log(`${currentUser}! ${welcomeMessage}`)
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+console.log(excitedWelcomeMessage)
+const shortGreeting = "Welcome, E!"
+console.log(shortGreeting)
